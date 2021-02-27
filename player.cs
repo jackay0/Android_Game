efc86ca5a231b9460cfe964bf3188726d3aa55eb
jackay0 <59718608+jackay0@users.noreply.github.com>
2021-02-27 15:18:55 -0600

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //private Player_Base playerbase;
+    private Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerBase = gameObject.GetComponent<Player_Base>();
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                float jumpVelocity = 4f;
+                rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+
+            }
+        }
+    }
+}
