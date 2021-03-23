0a2ac8948e4806a4c097a2b73723617ada4f8ed1
jackay0 <59718608+jackay0@users.noreply.github.com>
2021-03-23 08:19:09 -0500

    Add files via upload

---
+++ PlatformGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading; 
+
+public class PlatformGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform Platforms_too;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn(new Vector3(0,1));
+        Spawn(new Vector3(10,1));
+        
+    }
+
+    private void Spawn(Vector3 pos)
+    {
+        Instantiate(Platforms_too, pos,Quaternion.identity);
+    }
+    
+    void Update()
+    {
+        for(int i = 20;i<100;i=i+10)
+        {
+           Spawn(new Vector3(i,1)); 
+           Thread.Sleep(10000);
+        }
+
+
+    }
+}
--- player.cs
+++ player.cs
@@ -1,34 +1,88 @@
-﻿using System.Collections;
+using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    //private Player_Base playerbase;
+    
     private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+    
+    //Transform transform = Camera.main.transform;
+    
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        //playerBase = gameObject.GetComponent<Player_Base>();
+        
         rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount == 1)
+        float jumpVelocity = 0f;
+        rigidbody2d.velocity = new Vector2(10.0f,rigidbody2d.velocity.y);
+
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, groundLayer);
+        if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-
-            if(touch.phase == TouchPhase.Began)
+            
+            
+            if(raycastHit2d && touch.phase == TouchPhase.Began)
             {
-                float jumpVelocity = 4f;
-                rigidbody2d.velocity = Vector2.up * jumpVelocity;
-            }
-            if(touch.phase == TouchPhase.Moved)
-            {
+                
+                
+                
+                LayerMask x = LayerMask.GetMask("green");
+                LayerMask y = LayerMask.GetMask("yellow");
+                LayerMask z = LayerMask.GetMask("red");
 
+
+                
+
+                if(Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, x)){
+                    jumpVelocity = 20f;
+                    anim.SetBool("isJumping",true);
+                }
+                else if(Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, y)){
+                    jumpVelocity = 15f;
+                    anim.SetBool("isJumping",true);
+                }
+                else if(Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, z)){
+                    jumpVelocity = 10f;
+                    anim.SetBool("isJumping",true);
+                }
+                 
+                
+                
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x,jumpVelocity);
+                
+            
             }
+            
+               
+                
+            
         }
+        else{
+
+            anim.SetBool("isJumping",false);
+        }
+    
     }
+    
+    
+
+   
 }
