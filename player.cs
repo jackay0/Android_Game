b20506407ac441162b50200d2e0f9ba68e8ef455
jackay0 <59718608+jackay0@users.noreply.github.com>
2021-05-10 11:23:18 -0500

    Add files via upload

--- player.cs
+++ player.cs
@@ -18,11 +18,17 @@ public class player : MonoBehaviour
     public LayerMask groundLayer;
     private bool isTouchingGround;
 
+    public Vector2 positioner;
     private Animator anim;
 
     private int i;
 
     [SerializeField] private Transform stars;
+    [SerializeField] private Transform popup_high;
+    [SerializeField] private Transform popup_low;
+    [SerializeField] private Transform popup_uhoh;
+
+     public Sprite[] array;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +37,7 @@ public class player : MonoBehaviour
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
         xPos = rigidbody2d.position.x;
+        GetComponent<SpriteRenderer>().sprite = array[2];
     }
 
     private void Spawn(Vector3 pos)
@@ -38,12 +45,25 @@ public class player : MonoBehaviour
         Instantiate(stars, pos,Quaternion.identity);
     }
    
+    private void Spawn_high(Vector3 pos)
+    {
+        Instantiate(popup_high,pos,Quaternion.identity);
+    }
+    private void Spawn_low(Vector3 pos)
+    {
+        Instantiate(popup_low,pos,Quaternion.identity);
+    }
+    private void Spawn_uhoh(Vector3 pos)
+    {
+        Instantiate(popup_uhoh,pos,Quaternion.identity);
+    }
     // Update is called once per frame
     void Update()
     {
         i++;
         float jumpVelocity = 0f;
         rigidbody2d.velocity = new Vector2(9.7f,rigidbody2d.velocity.y);
+        positioner = new Vector2(rigidbody2d.position.x+12,rigidbody2d.position.y+3);
         
         if(i%30==0){
         Spawn(rigidbody2d.position);
@@ -70,20 +90,23 @@ public class player : MonoBehaviour
                 if(Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, x)){
                     ScoreScript.scoreValue += 1;
                     GetComponent<AudioSource>().Play();
-                    jumpVelocity = 50f;
+                    jumpVelocity = 47f;
                     anim.SetBool("isJumping",true);
+                    Spawn_high(positioner);
                 }
                 else if(Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, y)){
                     ScoreScript.scoreValue += 1;
                     GetComponent<AudioSource>().Play();
                     jumpVelocity = 25f;
                     anim.SetBool("isJumping",true);
+                    Spawn_uhoh(positioner);
                 }
                 else if(Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 1f, z)){
                     ScoreScript.scoreValue += 1;
                     GetComponent<AudioSource>().Play();
                     jumpVelocity = 11f;
                     anim.SetBool("isJumping",true);
+                    Spawn_low(positioner);
                 }
                  
                 
@@ -102,12 +125,18 @@ public class player : MonoBehaviour
             anim.SetBool("isJumping",false);
         }
 
-        if(rigidbody2d.position.y<-4.5f)
+        if(rigidbody2d.position.y<-4.2f)
         {
             RestartGame();
         }
     }
-    
+    public bool costumeChange(){
+
+        //if(){
+
+        //}
+        return false;
+    }
     public void RestartGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
